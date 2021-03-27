094f57da654e06018d39100863a0b40b2990df0e
amir00700 <43377462+amir00700@users.noreply.github.com>
2021-03-27 17:37:37 -0400

    form completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Task from "./components/Task";
+import TasksPage from "./components/TasksPage";
+// import TasksList from "./components/TasksList";
+// import Task from "./components/Task";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  
+    <TasksPage/>
+
+   
+    </>
   );
 }
 
---
+++ src/components/Task.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Task = () => {
+    return (
+        <div>
+            <h2>Task Components!!!</h2>
+        </div>
+    );
+};
+
+export default Task
